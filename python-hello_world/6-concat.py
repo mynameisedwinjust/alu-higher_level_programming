12dbad639f8558f3ab1aaf5a3a8164969fcbce11
mynameisedwinjust <e.muhire@alustudent.com>
2026-07-15 01:17:05 -0700

    Fix 6-concat.py using actual starter code

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton School!"
-message = f"{str1} {str2}"
-print(message)
+str1 = "Holberton"
+str2 = "School"
+str1 = f"{str1} {str2}"
+print(f"Welcome to {str1}!")
